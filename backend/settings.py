1e0bc910f5831eecbb3ee7df07f8836fd67311af
Alejandro <ThinkRamen@gmail.com>
2023-05-19 23:12:22 -0700

    supabase works its just empty

--- backend/settings.py
+++ backend/settings.py
@@ -124,19 +124,19 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'HOST': os.environ.get('SUPABASE_HOST'),
-        'PASSWORD': os.environ.get('SUPABASE_PW'),
-        'PORT': 5432,
-        'USER': 'postgres',
-        'CERT': os.environ.get('CA_CERT')
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'postgres',
+    #     'HOST': os.environ.get('SUPABASE_HOST'),
+    #     'PASSWORD': os.environ.get('SUPABASE_PW'),
+    #     'PORT': 5432,
+    #     'USER': 'postgres',
+    #     'CERT': os.environ.get('CA_CERT')
+    # }
 }
 
 # Password validation
